9e372c0a0d5e148df0eef3588e416039ba9bdfbc
edisonQuiceno <equiceno@student.fdu.edu>
2018-10-17 00:55:52 -0400

    - Added 4 default difficulties (easy, normal, hard, expert) - Added a custom difficulty. Users can adjust the game to their liking - Modularized the code. Made it more readable

--- Match to Pass/Match to Pass/Source.cpp
+++ Match to Pass/Match to Pass/Source.cpp
@@ -4,23 +4,32 @@
 #include <ctime>
 using namespace std;
 
-const int NUM_TOTAL_COLORS = 5;
-const int NUM_COLOR_COMBINATION = 3;
-const int NUM_GUESSES = 10;
+const int NUM_DEFAULT_TOTAL_COLORS = 13;
+const string DEFAULT_COLOR_OPTIONS[NUM_DEFAULT_TOTAL_COLORS] = { "Black", "Blue", "Brown", "Cyan", "Green", "Grey", "Magenta", "Orange", "Pink", "Purple", "Red", "White", "Yellow" };
+const int numColorCombinationS_EASY_DIFFICULTY = 3;
+const int numGuesses_EASY_DIFFICULTY = 10;
+const int numColorCombinationS_NORMAL_DIFFICULTY = 7;
+const int numGuesses_NORMAL_DIFFICULTY = 20;
+const int numColorCombinationS_HARD_DIFFICULTY = 10;
+const int numGuesses_HARD_DIFFICULTY = 30;
+const int numColorCombinationS_EXPERT_DIFFICULTY = 13;
+const int numGuesses_EXPERT_DIFFICULTY = 40;
 const int NUM_WAIT_SECONDS = 4;
+
+void chooseDifficulty(int&, int&, int&, string&, string*&);
+void playGame(int, int, int, string*);
+void displayRules();
 
 int main()
 {
-	int correctGuesses;
-	int guess;
-	string option;
-	string buffer;
-	string colorOptions[NUM_TOTAL_COLORS] = { "Red", "Green", "Blue", "Yellow", "Purple" };
-	string colorCombination[NUM_COLOR_COMBINATION];
-	string userColorGuess[NUM_COLOR_COMBINATION];
-	clock_t startClock;
+	int totalColors;
+	int numColorCombinations;
+	int numGuess;
+	string optionMainMenu, optionDifficulty;
+	string *customUserColors;
+	clock_t startClockExitProgram;
 
-	cout << "Match to Pass" << endl << endl;
+	cout << "Color Match Game" << endl << endl;
 
 	do
 	{
@@ -28,100 +37,224 @@ int main()
 		cout << "2. Read the Rules!" << endl;
 		cout << "Press anything else to exit the game" << endl;
 
-		getline(cin, option);
+		getline(cin, optionMainMenu);
 
-		if (option == "1")
+		cout << endl;
+
+		if (optionMainMenu == "1")
 		{
-			srand(time(0));
-			for (int i = 0; i < NUM_COLOR_COMBINATION; i++)
-				colorCombination[i] = colorOptions[rand() % NUM_TOTAL_COLORS];
+			customUserColors = nullptr;
+			chooseDifficulty(totalColors, numColorCombinations, numGuess, optionDifficulty, customUserColors);
+			if (optionDifficulty == "1" || optionDifficulty == "2" || optionDifficulty == "3" || optionDifficulty == "4" || optionDifficulty == "5")
+				playGame(totalColors, numColorCombinations, numGuess, customUserColors);
 
-			/* TESTING PURPOSES ONLY. PRINTS THE CORRECT COLOR COMBINATION
-			
-			for (int i = 0; i < NUM_COLOR_COMBINATION; i++)
-				cout << colorCombination[i] << " ";
-			cout << endl;
-			*/
-
-			for (int i = 0; i < NUM_GUESSES; i++)
-			{
-				cout << endl << "Enter your guess: ";
-
-				for (int j = 0; j < NUM_COLOR_COMBINATION; j++)
-					cin >> userColorGuess[j];
-
-				getline(cin, buffer);
-
-				correctGuesses = 0;
-				for (int j = 0; j < NUM_COLOR_COMBINATION; j++)
-				{
-					if (userColorGuess[j] != colorCombination[j])
-						break;
-					correctGuesses++;
-				}
-
-				if (correctGuesses == NUM_COLOR_COMBINATION)
-					break;
-
-				cout << endl;
-
-				for (int j = 0; j < NUM_COLOR_COMBINATION; j++)
-				{
-					if (userColorGuess[j] == colorCombination[j])
-					{
-						cout << "Color " << j << " is one of the right colors and in the right position" << endl;
-						continue;
-					}
-
-					guess = 0;
-					for (int k = 0; k < NUM_COLOR_COMBINATION; k++)
-					{
-						if (userColorGuess[j] == colorCombination[k])
-						{
-							cout << "Color " << j << " is one of the right colors, but its not in the right position" << endl;
-							break;
-						}
-						guess++;
-					}
-
-					if (guess == NUM_COLOR_COMBINATION)
-						cout << "Color " << j << " is not one of the right colors" << endl;
-				}
-			}
-
-			cout << endl;
-
-			if (correctGuesses == NUM_COLOR_COMBINATION)
-				cout << "You got the right combination!!" << endl << "Winner Winner Chicken Dinner!!" << endl;
-
-			else
-				cout << "You lose..." << endl << "Try again next time!" << endl;
-
-			cout << endl << "Returning to Main Menu..." << endl << endl;
-
-			startClock = clock();
-			while ((clock() - startClock) / CLOCKS_PER_SEC < NUM_WAIT_SECONDS) {}
+			delete[] customUserColors;
 		}
 
-		else if (option == "2")
-		{
-			cout << endl << "---------------------------------- Rules ----------------------------------" << endl << endl;
-			cout << "1. Enter the " << NUM_COLOR_COMBINATION << " color combination that you think is correct and you win!" << endl;
-			cout << "2. The only colors you can guess are ";
-			for (int i = 0; i < NUM_TOTAL_COLORS; i++)
-				cout << colorOptions[i] << ", ";
-			cout << "\b\b." << endl;
-			cout << "3. Type the colors in succession. Ex: Blue Green Yellow." << endl;
-			cout << "4. You get " << NUM_GUESSES << " guesses. After the " << NUM_GUESSES << "th incorrect guess, you lose!" << endl << endl;
-		}
+		else if (optionMainMenu == "2")
+			displayRules();
 
-	} while (option == "1" || option == "2");
+	} while (optionMainMenu == "1" || optionMainMenu == "2");
 
-	cout << endl << "Thank you for playing!" << endl << endl;
+	cout << "Thank you for playing!" << endl << endl;
 	cout << "Exiting...";
 
-	startClock = clock();
-	while ((clock() - startClock) / CLOCKS_PER_SEC < NUM_WAIT_SECONDS) {}
+	startClockExitProgram = clock();
+	while ((clock() - startClockExitProgram) / CLOCKS_PER_SEC < NUM_WAIT_SECONDS) {}
 
 	return 0;
+}
+
+void chooseDifficulty(int& totalColors, int& numColorCombinations, int& numGuesses, string& option, string *&customUserColors)
+{
+	string tempInput;
+
+	cout << "Choose your difficulty level:" << endl << endl;
+	cout << "1. Easy" << endl;
+	cout << "2. Normal" << endl;
+	cout << "3. Hard" << endl;
+	cout << "4. Expert" << endl;
+	cout << "5. Custom" << endl;
+	cout << "Press anything else to return to the main menu" << endl;
+
+	getline(cin, option);
+
+	cout << endl;
+
+	if (option == "1")
+	{
+		numColorCombinations = numColorCombinationS_EASY_DIFFICULTY;
+		numGuesses = numGuesses_EASY_DIFFICULTY;
+	}
+
+	else if (option == "2")
+	{
+		numColorCombinations = numColorCombinationS_NORMAL_DIFFICULTY;
+		numGuesses = numGuesses_NORMAL_DIFFICULTY;
+	}
+
+	else if (option == "3")
+	{
+		numColorCombinations = numColorCombinationS_HARD_DIFFICULTY;
+		numGuesses = numGuesses_HARD_DIFFICULTY;
+	}
+
+	else if (option == "4")
+	{
+		numColorCombinations = numColorCombinationS_EXPERT_DIFFICULTY;
+		numGuesses = numGuesses_EXPERT_DIFFICULTY;
+	}
+
+	else if (option == "5")
+	{
+		cout << "Enter the total number of colors: ";
+		getline(cin, tempInput);
+		while (atoi(tempInput.c_str()) <= 0)
+		{
+			cout << "Input must be a positive integer: ";
+			getline(cin, tempInput);
+		}
+		totalColors = atoi(tempInput.c_str());
+
+		customUserColors = new string[totalColors];
+
+		cout << "Enter the number of color combinations you want to pick: ";
+		getline(cin, tempInput);
+		while (atoi(tempInput.c_str()) <= 0 || atoi(tempInput.c_str()) > totalColors)
+		{
+			cout << "Input must be a positive integer and less than or equal to the total number of colors: ";
+			getline(cin, tempInput);
+		}
+		numColorCombinations = atoi(tempInput.c_str());
+
+		cout << "Enter the number of guesses you can use: ";
+		getline(cin, tempInput);
+		while (atoi(tempInput.c_str()) <= 0)
+		{
+			cout << "Input must be a positive integer: ";
+			getline(cin, tempInput);
+		}
+		numGuesses = atoi(tempInput.c_str());
+
+		cout << "Enter the " << totalColors << " colors in succession: ";
+		for (int i = 0; i < totalColors; i++)
+			cin >> customUserColors[i];
+
+		getline(cin, tempInput);
+
+		cout << endl;
+	}
+}
+void playGame(int totalColors, int numColorCombination, int numGuesses, string* customUserColors)
+{
+	int correctGuesses;
+	int guess;
+	string buffer;
+	string *colorCombination = new string[numColorCombination];
+	string *userColorGuess = new string[numColorCombination];
+	clock_t startClockExitGame;
+
+	cout << "All the possible colors are ";
+
+	srand(time(0));
+
+	if (!customUserColors)
+	{
+		for (int i = 0; i < numColorCombination; i++)
+			colorCombination[i] = DEFAULT_COLOR_OPTIONS[rand() % NUM_DEFAULT_TOTAL_COLORS];
+
+		for (int i = 0; i < NUM_DEFAULT_TOTAL_COLORS; i++)
+			cout << DEFAULT_COLOR_OPTIONS[i] << ", ";
+	}
+
+	else
+	{
+		for (int i = 0; i < numColorCombination; i++)
+			colorCombination[i] = customUserColors[rand() % totalColors];
+
+		for (int i = 0; i < totalColors; i++)
+			cout << customUserColors[i] << ", ";
+	}
+
+	cout << "\b\b\b" << endl;
+
+	cout << "In this difficulty, there are " << numColorCombination << " colors in the ordered combination and you are given " << numGuesses << " guesses." << endl;
+
+	/* TESTING PURPOSES ONLY. PRINTS THE CORRECT COLOR COMBINATION
+	
+	for (int i = 0; i < numColorCombination; i++)
+		cout << colorCombination[i] << " ";
+	cout << endl;
+	*/
+
+	for (int i = 0; i < numGuesses; i++)
+	{
+		cout << endl << "Enter your guess: ";
+
+		for (int j = 0; j < numColorCombination; j++)
+			cin >> userColorGuess[j];
+
+		getline(cin, buffer);
+
+		correctGuesses = 0;
+		for (int j = 0; j < numColorCombination; j++)
+		{
+			if (userColorGuess[j] != colorCombination[j])
+				break;
+			correctGuesses++;
+		}
+
+		if (correctGuesses == numColorCombination)
+			break;
+
+		cout << endl;
+
+		for (int j = 0; j < numColorCombination; j++)
+		{
+			if (userColorGuess[j] == colorCombination[j])
+			{
+				cout << "Color " << j + 1 << " is in the right position" << endl;
+				continue;
+			}
+
+			guess = 0;
+			for (int k = 0; k < numColorCombination; k++)
+			{
+				if (userColorGuess[j] == colorCombination[k])
+				{
+					cout << "Color " << j + 1 << " is one of the right colors, but its not in the right position" << endl;
+					break;
+				}
+				guess++;
+			}
+
+			if (guess == numColorCombination)
+				cout << "Color " << j + 1 << " is not one of the right colors" << endl;
+		}
+	}
+
+	cout << endl;
+
+	if (correctGuesses == numColorCombination)
+		cout << "You got the right combination!!" << endl << "Winner Winner Chicken Dinner!!" << endl;
+
+	else
+		cout << "You lose..." << endl << "Try again next time!" << endl;
+
+	delete[] colorCombination;
+	delete[] userColorGuess;
+
+	cout << endl << "Returning to Main Menu..." << endl << endl;
+
+	startClockExitGame = clock();
+	while ((clock() - startClockExitGame) / CLOCKS_PER_SEC < NUM_WAIT_SECONDS) {}
+}
+
+void displayRules()
+{
+	cout << "---------------------------------- Rules ----------------------------------" << endl << endl;
+	cout << "1. Enter the color combination in the right order and you win!" << endl;
+	cout << "2. You will be given a list of all possible color choices when you select a difficulty" << endl;
+	cout << "3. Type the colors in succession, seperating each color with one whitespace. Ex: Blue Green Yellow Red." << endl;
+	cout << "4. You will be given a number of guesses you can use. If you use up all your guesses before finding the correct color combination, you lose!" << endl << endl;
 }
