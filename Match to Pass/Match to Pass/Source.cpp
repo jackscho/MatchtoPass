54e780fbd0982ad00f7fe5bdcf1595451da0cc98
edisonQuiceno <equiceno@student.fdu.edu>
2018-10-17 00:27:49 -0400

    - Variable names now better describe their function - Only one array holds the strings of colors - "exit(0)" is no longer used. Program will reach "return 0" no mattter what path the code takes - Made section of code that determines the color combination and number of guesses more dynamic. This means the color names, number of colors in the combination, and the number of guesses can be easily changed without changing a lot of code - Added a timer (in seconds) when returning to the main menu and exiting the program - Changed possible colors to Red, Green, Blue, Yellow, and Purple

--- Match to Pass/Match to Pass/Source.cpp
+++ Match to Pass/Match to Pass/Source.cpp
@@ -1,160 +1,127 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+const int NUM_TOTAL_COLORS = 5;
+const int NUM_COLOR_COMBINATION = 3;
+const int NUM_GUESSES = 10;
+const int NUM_WAIT_SECONDS = 4;
 
 int main()
 {
-	string g1, g2, g3;
+	int correctGuesses;
+	int guess;
+	string option;
+	string buffer;
+	string colorOptions[NUM_TOTAL_COLORS] = { "Red", "Green", "Blue", "Yellow", "Purple" };
+	string colorCombination[NUM_COLOR_COMBINATION];
+	string userColorGuess[NUM_COLOR_COMBINATION];
+	clock_t startClock;
 
-	int a;
-	const int ARRAY_SIZE = 5;
-	srand(time(0));
-	string m1[ARRAY_SIZE] = { "Green","Blue", "Yellow" ,"Orange", "Pink" };
-	string m2[ARRAY_SIZE] = { "Green","Blue", "Yellow", "Orange", "Pink" };
-	string m3[ARRAY_SIZE] = { "Green","Blue", "Yellow", "Orange", "Pink" };
+	cout << "Match to Pass" << endl << endl;
 
-	cout << "1. Play the Game!" << endl;
-	cout << "2. Read the Rules!" << endl;
-	cout << "3. Exit." << endl;
-
-	cin >> a;
-
-	if (a == 2)
+	do
 	{
-		cout << "Rules" << endl << "_________________________________" << endl << "1. The only colors you can guess are Blue, Yellow, Orange, Green, and Pink." << endl;
-		cout << "2. Type 3 colors in succession that you think is the correct combination. Ex: Blue Green Yellow. " << endl;
-		cout << "3. You get 10 guesses. After the 10th incorrect guess, you lose!" << endl;
-		cout << "4. Enter the correct combination and you win." << endl;
+		cout << "1. Play the Game!" << endl;
+		cout << "2. Read the Rules!" << endl;
+		cout << "Press anything else to exit the game" << endl;
 
-		cout << "Press 1 to play the game ";
+		getline(cin, option);
 
-
-		cin >> a;
-	}
-	else if (a == 3)
-	{
-		exit(0);
-	}
-
-	do {
-		if (a == 1)
+		if (option == "1")
 		{
-			for (int i = 0; i < 1; i++)
+			srand(time(0));
+			for (int i = 0; i < NUM_COLOR_COMBINATION; i++)
+				colorCombination[i] = colorOptions[rand() % NUM_TOTAL_COLORS];
+
+			/* TESTING PURPOSES ONLY. PRINTS THE CORRECT COLOR COMBINATION
+			
+			for (int i = 0; i < NUM_COLOR_COMBINATION; i++)
+				cout << colorCombination[i] << " ";
+			cout << endl;
+			*/
+
+			for (int i = 0; i < NUM_GUESSES; i++)
 			{
-				int index = rand() % ARRAY_SIZE;
-				m1[i] = m1[index];
-				int index1 = rand() % ARRAY_SIZE;
-				m2[i] = m2[index1];
-				int index2 = rand() % ARRAY_SIZE;
-				m3[i] = m3[index2];
+				cout << endl << "Enter your guess: ";
+
+				for (int j = 0; j < NUM_COLOR_COMBINATION; j++)
+					cin >> userColorGuess[j];
+
+				getline(cin, buffer);
+
+				correctGuesses = 0;
+				for (int j = 0; j < NUM_COLOR_COMBINATION; j++)
+				{
+					if (userColorGuess[j] != colorCombination[j])
+						break;
+					correctGuesses++;
+				}
+
+				if (correctGuesses == NUM_COLOR_COMBINATION)
+					break;
+
+				cout << endl;
+
+				for (int j = 0; j < NUM_COLOR_COMBINATION; j++)
+				{
+					if (userColorGuess[j] == colorCombination[j])
+					{
+						cout << "Color " << j << " is one of the right colors and in the right position" << endl;
+						continue;
+					}
+
+					guess = 0;
+					for (int k = 0; k < NUM_COLOR_COMBINATION; k++)
+					{
+						if (userColorGuess[j] == colorCombination[k])
+						{
+							cout << "Color " << j << " is one of the right colors, but its not in the right position" << endl;
+							break;
+						}
+						guess++;
+					}
+
+					if (guess == NUM_COLOR_COMBINATION)
+						cout << "Color " << j << " is not one of the right colors" << endl;
+				}
 			}
 
-			for (int i = 0; i < 1; i++)
-			{
-				cout << m1[i] << " ";
-				cout << " " << m2[i] << " ";
-				cout << " " << m3[i] << " ";
-			}
 			cout << endl;
 
-			cout << "Enter your guess (Pick between Green, Orange, Pink, Blue, and Yellow)" << endl;
-			for (int n = 0; n < 3; n++)
-			{
-				cin >> g1 >> g2 >> g3;
+			if (correctGuesses == NUM_COLOR_COMBINATION)
+				cout << "You got the right combination!!" << endl << "Winner Winner Chicken Dinner!!" << endl;
 
-				if (g1 == m1[0] && g2 == m2[0] && g3 == m3[0])
-				{
-					cout << "You got the right combination!!" << endl;
-					cout << "Winner Winner Chicken Dinner!!" << endl;
+			else
+				cout << "You lose..." << endl << "Try again next time!" << endl;
 
-					cout << "Do you want to play again? Press 1. Press 2 to Exit." << endl;
+			cout << endl << "Returning to Main Menu..." << endl << endl;
 
-					n = 3;
-					cin >> a;
-
-				}
-
-				else if (g1 == m1[0] && g2 == m2[0])
-				{
-					cout << "Color 1" << " and " << "Color 2" << " are in the right position and the right color!" << endl;
-				}
-				else if (g1 == m1[0] && g3 == m3[0])
-				{
-					cout << "Color 1" << " and " << "Color 3" << " are in the right position and the right color!" << endl;
-				}
-				else if (g2 == m2[0] && g3 == m3[0])
-				{
-					cout << "Color 2" << " and " << "Color 3" << " are in the right position and the right color!" << endl;
-				}
-
-				else if ((g1 == m2[0] || g1 == m3[0]) && (g2 == m1[0] || g2 == m3[0]) && (g3 == m1[0] || g3 == m2[0]))
-				{
-					if (g1 == m1[0])
-					{
-						cout << "Color 1 is correct, the other 2 are the right colors but wrong position" << endl;
-					}
-					else if (g2 == m2[0])
-					{
-						cout << "Color 2 is correct, the other 2 are the right colors but wrong position" << endl;
-					}
-					else if (g3 == m3[0])
-					{
-						cout << "Color 3 is correct, the other 2 are the right colors but wrong position" << endl;
-					}
-					else
-					{
-						cout << "Colors are correct but in the wrong position!" << endl;
-					}
-				}
-				else if (g1 == m2[0] && g2 == m3[0])
-				{
-					cout << "Color 1" << " and " << "Color 2" << " are the right color but in the wrong position" << endl;
-				}
-				else if (g1 == m3[0] && g2 == m1[0])
-				{
-					cout << "Color 1" << " and " << "Color 2" << " are the right color but in the wrong position" << endl;
-				}
-
-				else if (g1 == m2[0] && g3 == m1[0])
-				{
-					cout << "Color 1" << " and " << "Color 3" << " are the right color but in the wrong position" << endl;
-				}
-				else if (g1 == m3[0] && g3 == m2[0])
-				{
-					cout << "Color 1" << " and " << "Color 3" << " are the right color but in the wrong position" << endl;
-				}
-
-				else if (g1 == m1[0] || g2 == m2[0] || g3 == m3[0])
-				{
-					cout << "1 Is the right color and position" << endl;
-				}
-				else if (g1 == m2[0] || g1 == m3[0] || g2 == m1[0] || g2 == m3[0] || g3 == m1[0] || g3 == m2[0])
-				{
-					cout << "1 is the right color, wrong position." << endl;
-				}
-
-				else if (g1 != m1[0] && g1 != m2[0] && g1 != m3[0] && g2 != m1[0] && g2 != m2[0] && g3 != m3[0] && g3 != m1[0] && g3 != m2[0] && g3 != m3[0])
-				{
-					cout << "None of the colors are the right color or the right position :(" << endl;
-				}
-			}
-			cout << endl;
-
-			cout << "You lose!" << endl;
-			cout << "Do you want to play again? Press 1. Press 2 to Exit." << endl;
-
-			cin >> a;
+			startClock = clock();
+			while ((clock() - startClock) / CLOCKS_PER_SEC < NUM_WAIT_SECONDS) {}
 		}
-	} while (a == 1);
 
-	if (a == 2)
-	{
-		exit(0);
-	}
+		else if (option == "2")
+		{
+			cout << endl << "---------------------------------- Rules ----------------------------------" << endl << endl;
+			cout << "1. Enter the " << NUM_COLOR_COMBINATION << " color combination that you think is correct and you win!" << endl;
+			cout << "2. The only colors you can guess are ";
+			for (int i = 0; i < NUM_TOTAL_COLORS; i++)
+				cout << colorOptions[i] << ", ";
+			cout << "\b\b." << endl;
+			cout << "3. Type the colors in succession. Ex: Blue Green Yellow." << endl;
+			cout << "4. You get " << NUM_GUESSES << " guesses. After the " << NUM_GUESSES << "th incorrect guess, you lose!" << endl << endl;
+		}
 
-	cout << endl;
-	system("pause");
+	} while (option == "1" || option == "2");
+
+	cout << endl << "Thank you for playing!" << endl << endl;
+	cout << "Exiting...";
+
+	startClock = clock();
+	while ((clock() - startClock) / CLOCKS_PER_SEC < NUM_WAIT_SECONDS) {}
+
 	return 0;
 }
